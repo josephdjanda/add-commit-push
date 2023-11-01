f3031f6cf7182fd9d95aaa4aec93949d62c98861
janda <jjanda@lewisu.edu>
2023-11-01 11:27:39 -0500

    update files.

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,1 +1,12 @@
-print("Starting add-commit-push")
+import os
+
+print('add commit push')
+print('\ngit status')
+os.system('git status')
+
+print('\ngit add -A')
+os.system('git add -A')
+print('\ngit commit -m "update files."')
+os.system('git commit -m "update files."')
+print('\ngit psuh')
+os.system('git push')
