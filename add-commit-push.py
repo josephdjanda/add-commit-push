a440b27d35264ddcda68f41813a06d5bb9d19580
janda <jjanda@lewisu.edu>
2023-11-03 11:23:49 -0500

    Update files.

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,4 +1,10 @@
 import os
+import sys
+
+print('number of arguments:')
+print(len(sys.argv))
+print('Argument List:')
+print(str(sys.argv))
 
 print('add commit push')
 print('\ngit status')
@@ -6,7 +12,7 @@ os.system('git status')
 
 print('\ngit add -A')
 os.system('git add -A')
-print('\ngit commit -m "update files."')
-os.system('git commit -m "update files."')
-print('\ngit psuh')
+print('\ngit commit -m "Update files."')
+os.system('git commit -m "Update files."')
+print('\ngit push')
 os.system('git push')
